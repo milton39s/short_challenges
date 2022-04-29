907a803d30f3616452f4991a26cc51a935335499
Milton <milton39s_@hotmail.com>
2022-04-29 16:22:48 -0500

    codeSignal.ipynb added

---
+++ debbuger.py
@@ -0,0 +1,14 @@
+
+
+
+def my_function(): 
+    return "another thing"
+
+
+if __name__ == "__main__":
+
+    print("something")
+
+    a = my_function()
+
+    print("here we go again")
--- testing/TestCommand.py
+++ testing/TestCommand.py
@@ -1,5 +1,5 @@
 import pytest
-from app.test_exercise import admin_command
+from app1.test_exercise import admin_command
 
 class TestCommand:
 
